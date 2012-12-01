307330ffb20f50179e48cbb26d32e788b48e0962
rickipsgx <rickipsgx@hotmail.com>
2012-12-01 12:29:02 -0500

    bnbn

--- programacion-variada/README.md
+++
@@ -1,5 +0,0 @@
-ProgVariada
-===========
-
-Esta es el sitio web de programacion variada
-lo estoy trabajando con Ricardo
--- programacion-variada/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include('php/config.php');
-	
-	include('php/header.php');
-	include('php/index.php');
-	include('php/footer.php');
-?>
